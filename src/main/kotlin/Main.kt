961f5718f321130155a36a51906ad2ade6d8bb6c
Eugene Otieno <otienoeo@gmail.com>
2023-01-14 20:05:43 +0300

    Added more options

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,46 @@
 fun main() {
     val taskList = mutableListOf<String>()
-    println("Input the tasks (enter a blank line to end):")
-    var inputList = true
-    while (inputList) {
-        val taskInput = readln().trim()
-        if (taskInput.isBlank()) {
-            inputList = false
-        } else {
-            val num = taskList.size + 1
-            if (num <= 9) {
-                taskList.add("$num  $taskInput")
-            } else {
-                taskList.add("$num $taskInput")
+    var end = false
+    do {
+        println("Input an action (add, print, end):")
+        when(readln().lowercase()) {
+            "add" -> {
+                println("Input a new task (enter a blank line to end):")
+                var inputList = true
+                var task = ""
+                while (inputList) {
+                    val taskInput = readln().trim()
+                    if (taskInput.isBlank()) {
+                        if (task.isNotBlank()) taskList.add(task)
+                        if (task.isBlank()) println("The task is blank")
+                        inputList = false
+                    } else {
+                        val num = taskList.size + 1
+                        if (num <= 9) {
+                            if (task.isBlank())  task = "$num  $taskInput"
+                            else  task += "\n   $taskInput"
+                        } else {
+                            if (task.isBlank())  task = "$num $taskInput"
+                            else  task += "\n  $taskInput"
+                        }
+                    }
+                }
             }
+            "print" -> {
+                if (taskList.isEmpty()) {
+                    println("No tasks have been input")
+                } else {
+                    for (task in taskList) {
+                        println(task)
+                        println()
+                    }
+                }
+            }
+            "end" -> {
+                println("Tasklist exiting!")
+                end = true
+            }
+            else -> println("The input action is invalid")
         }
-    }
-
-    if (taskList.isEmpty()) {
-        println("No tasks have been input")
-    } else {
-        for (task in taskList) {
-            println(task)
-        }
-    }
+    } while (!end)
 }
